37cad3f8ac38668f87904e83cf966411e80581f5
mahacoder <ananthan31@gmail.com>
2018-01-18 19:26:43 -0800

    Added code to tokenize and count frequencies of words in a file

---
+++ crawler.py
@@ -0,0 +1,34 @@
+import argparse
+from collections import defaultdict
+
+separator = ' '
+
+def compute_word_frequencies(tokens):
+    frequencies = defaultdict(int)
+    for token in tokens:
+        frequencies[str.lower(token)]+=1
+
+    return frequencies
+
+def tokenize(text_file_path):
+    tokens = []
+    with open(text_file_path) as file:
+        for line in file:
+            words = line.split(separator)
+            tokens.extend(words)
+            # for word in words: tokens[str.lower(word)]+=1
+    return tokens
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filepath", help="path of the file to tokenize")
+    args = parser.parse_args()
+    filepath = args.filepath
+    tokens = tokenize(filepath)
+    word_frequencies = compute_word_frequencies(tokens)
+    print(word_frequencies)
+
+
+if __name__=='__main__':
+    main()
