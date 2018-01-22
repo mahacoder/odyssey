b880ac3efaf00c3a146942c50437e1d9de83b7e4
mahacoder <ananthan31@gmail.com>
2018-01-21 19:11:07 -0800

    Moved comparing text files and frequency files into two source files

--- compare.py
+++ compare.py
@@ -4,25 +4,6 @@ from tokenizer import tokenize
 from tokenizer import compute_word_frequencies
 from tokenizer import print_sorted_order
 
-def combine_frequency_map(frequency_map1, frequency_map2):
-    map = defaultdict(int)
-    for k, v in frequency_map1.items():
-        map[k] = v
-    for k, v in frequency_map2.items():
-        if k in map: map[k]+=v
-        else: map[k]=v
-    return map
-
-def read_map(filepath):
-    map = defaultdict(int)
-    with open(filepath) as f:
-        for line in f:
-            k, v = line.split(',')
-            k, v = k.strip(), int(v.strip())
-            if k in map: map[k]+=v
-            else: map[k]=v
-    return map
-
 def count_common(frequency1, frequency2, print_common_keys=False):
     counter = 0
     for k, v in frequency1.items():
@@ -36,23 +17,20 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("filepath1", help="path of the file to compare")
     parser.add_argument("filepath2", help="path of the file to compare")
-    parser.add_argument("is_map", help="True if the files are frequency maps")
+    parser.add_argument("print_common", help="True to display common keys")
     args = parser.parse_args()
     filepath_1 = args.filepath1
     filepath_2 = args.filepath2
-    is_map = args.is_map
-    if is_map=='True' or is_map=='true':
-        word_frequencies_1 = read_map(filepath_1)
-        word_frequencies_2 = read_map(filepath_2)
-        word_frequency = combine_frequency_map(word_frequencies_1, word_frequencies_2)
-        print_sorted_order(word_frequency)
-    else:
-        tokens_1 = tokenize(filepath_1)
-        tokens_2 = tokenize(filepath_2)
-        word_frequencies_1 = compute_word_frequencies(tokens_1)
-        word_frequencies_2 = compute_word_frequencies(tokens_2)
+    print_common_flag = args.print_common
+    tokens_1 = tokenize(filepath_1)
+    tokens_2 = tokenize(filepath_2)
+    word_frequencies_1 = compute_word_frequencies(tokens_1)
+    word_frequencies_2 = compute_word_frequencies(tokens_2)
+    if print_common_flag in ['True', 'true', 'T', 't']:
         count = count_common(word_frequencies_1, word_frequencies_2, True)
-        print(count)
+    else:
+        count = count_common(word_frequencies_1, word_frequencies_2)
+    print(count)
 
 if __name__=='__main__':
     main()
---
+++ compare_freq_map.py
@@ -0,0 +1,39 @@
+import argparse
+from collections import defaultdict
+from tokenizer import tokenize
+from tokenizer import compute_word_frequencies
+from tokenizer import print_sorted_order
+
+def combine_frequency_map(frequency_map1, frequency_map2):
+    map = defaultdict(int)
+    for k, v in frequency_map1.items():
+        map[k] = v
+    for k, v in frequency_map2.items():
+        if k in map: map[k]+=v
+        else: map[k]=v
+    return map
+
+def read_map(filepath):
+    map = defaultdict(int)
+    with open(filepath) as f:
+        for line in f:
+            k, v = line.split(',')
+            k, v = k.strip(), int(v.strip())
+            if k in map: map[k]+=v
+            else: map[k]=v
+    return map
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("wordmap1", help="path of the word map to compare")
+    parser.add_argument("wordmap2", help="path of the word map to compare")
+    args = parser.parse_args()
+    filepath_1 = args.wordmap1
+    filepath_2 = args.wordmap2
+    word_frequencies_1 = read_map(filepath_1)
+    word_frequencies_2 = read_map(filepath_2)
+    word_frequency = combine_frequency_map(word_frequencies_1, word_frequencies_2)
+    print_sorted_order(word_frequency)
+
+if __name__=='__main__':
+    main()
