1257b0e8df842cee92d90f06103ed9be80f00184
mahacoder <ananthan31@gmail.com>
2018-03-10 15:40:24 -0800

    Initial implementation of tf_idf

--- project2/applications/search/crawler_frame.py
+++ project2/applications/search/crawler_frame.py
@@ -1,5 +1,11 @@
 import codecs
 import logging
+import string #added because of tf-idf
+import math #added because of tf-idf
+import sys
+sys.path.append('../')
+
+from tokenizer import tokenize 
 from datamodel.search.AsbapatApushpenKbaijalKyuseony_datamodel import AsbapatApushpenKbaijalKyuseonyLink, OneAsbapatApushpenKbaijalKyuseonyUnProcessedLink, add_server_copy, get_downloaded_content
 from spacetime.client.IApplication import IApplication
 from spacetime.client.declarations import Producer, GetterSetter, Getter, ServerTriggers
@@ -7,6 +13,7 @@ from lxml import html,etree
 import re, os
 from time import time
 from uuid import uuid4
+from __future__ import division #added because of tf-idf
 
 from urlparse import urlparse, parse_qs
 from uuid import uuid4
@@ -18,6 +25,8 @@ max_link_count = 0
 max_link_page = ''
 visited_count = 0
 redirect_count = 0
+inverted_index_list = {} #dictionary type
+PATH_T0_FILES = os.path.join(os.getcwd, 'files')
 
 @Producer(AsbapatApushpenKbaijalKyuseonyLink)
 @GetterSetter(OneAsbapatApushpenKbaijalKyuseonyUnProcessedLink)
@@ -58,6 +67,57 @@ class CrawlerFrame(IApplication):
             "Time time spent this session: ",
             time() - self.starttime, " seconds.")
 
+#tokenization function in the website
+tokenize = lambda doc: doc.lower().split(" ")
+
+#creating inverted index per term
+def create_inverted_index(term, document, position):
+
+    #if term is new, then make a new document list
+    if term in inverted_index_list == False:
+        document_list = {}
+
+        document_list[document] = position
+        inverted_index_list[term] = document_list
+
+    #if term is existed, then append to a document list
+    else:
+        inverted_index_list[term][document] = position
+
+#this function is converting all terms in the document into inverted index
+def create_document_inverted_list(tokens, document):
+    for term in tokens:
+        # position_list = []
+        position_list = [tokens.index(i) for i in tokens if term == tokens.index(i)]
+        create_inverted_index(term,document,position_list)
+
+#custructing part (whole)
+def construct_index(directory_path):
+    tokens_from_file = []
+    for file_path in os.listdir(directory_path): 
+         tokens_from_file = tokenize(os.path.join(directory_path. file_path))
+         create_document_inverted_list(tokens_from_file, file_path)
+
+#tf = count the number of positions> DONE
+def term_frequency(term, document_name):
+    return len(inverted_index_list[term][document_name])
+
+#in the website, they use this function and so as lecture
+def sublinear_term_frequency(term, document_name):
+    return 1 + math.log(term_frequency(term, document_name))
+
+#function for calculating idf
+def inverse_document_frequencies(term):
+    # pass directory name or make it global
+    file_list = os.listdir("directory name")#calcuate N
+    return math.log(len(file_list)/len(inverted_index_list[term]))
+
+#calculate the tfidf
+def tfidf(term):
+    idf = inverse_document_frequencies(term)
+    tf = sublinear_term_frequency(term, document)
+    return tf * idf
+
 def create_url(current_url, relative_url):
     global counter
     url = ''
@@ -78,11 +138,11 @@ def links_from_link(content, current_url, http_code):
     root_url = "http://www.ics.uci.edu"
     links = []
     if http_code==200 and len(content)==0:
-        return links
+        pass
         # with codecs.open('empty_html.txt', mode='a', encoding='utf-8') as html_file:
         #     html_file.write(current_url+'\n')
-    elif len(content)==0 or content is None:
-        return links
+    elif len(content)==0:
+        pass
         # with codecs.open('non_200.txt', mode='a', encoding='utf-8') as non_200:
         #     non_200.write(current_url+'|'+str(http_code)+'\n')
     else:
@@ -147,7 +207,7 @@ def is_valid(url):
     try:
         is_valid_flag = ".ics.uci.edu" in parsed.hostname \
             and not re.match(".*\.(css|js|bmp|gif|jpe?g|ico" + "|png|tiff?|mid|mp2|mp3|mp4"\
-            + "|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|txt|pdf" \
+            + "|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf" \
             + "|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso|epub|dll|cnf|tgz|sha1" \
             + "|thmx|mso|arff|rtf|jar|csv"\
             + "|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower()) \
