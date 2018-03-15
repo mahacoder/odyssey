3c90307dc07748744eb5eebe570de837c5e505de
Ananthakrishnan <ananthan31@gmail.com>
2018-03-15 00:05:22 -0700

    Added progress tracker while creating index

--- project2/applications/search/indexer.py
+++ project2/applications/search/indexer.py
@@ -8,6 +8,7 @@ import pickle
 import math
 import nltk
 from nltk.stem.porter import *
+from tqdm import tqdm
 
 sys.path.append(os.path.join(os.getcwd(), 'odyssey'))
 
@@ -17,6 +18,14 @@ class Indexer():
 
     def __init__(self):
         self.inverted_index_list = {}
+        try:
+            with open('index_tracker.p', 'r') as fp:
+                self.inverted_index_tracker = pickle.load(fp)
+        except:
+            self.inverted_index_tracker = set()
+
+    def add_to_index_tracker(self, processed_file):
+        self.inverted_index_tracker.add(processed_file)
 
     #creating inverted index per term
     def create_inverted_index(self, term, document, position_list):
@@ -41,18 +50,26 @@ class Indexer():
 
         tokens_from_file = []
         stemmer = PorterStemmer()
+        counter=0
         for root, dirs, files in os.walk(directory_path):
             if len(dirs)!=0:
                 continue # ignore the subdirectory without files
             dir_name = root[root.rindex('\\')+1:]
-            for file_ in files:
-                # file_name = print(dir_name+'/'+file_)
+            print('{} folders processed till now'.format(counter))
+            print('Process folder number {}'.format(dir_name))
+            for file_ in tqdm(files):
+                file_name = dir_name+'/'+file_
+                if file_name in self.inverted_index_tracker:
+                    continue
                 file_path = os.path.join(root, file_)
                 plurals = tokenize(file_path, use_nltk=True)
                 tokens_from_file = [stemmer.stem(tokens) for tokens in plurals]
                 self.create_document_inverted_list(tokens_from_file, file_path)
-
-        return tokens_from_file
+                self.add_to_index_tracker(file_name)
+                # print('{} processed'.format(file_name))
+                with open('index_tracker.p', 'wb') as fp:
+                    pickle.dump(self.inverted_index_tracker, fp, protocol=pickle.HIGHEST_PROTOCOL)
+            counter+=1
 
         # tokens_from_file = []
         # for file_path in os.listdir(directory_path):
@@ -104,8 +121,6 @@ if __name__=='__main__':
     start = time.time()
     indexer.construct_index(os.path.join(path_till_odyssey, pages_dir_name))
     print('Constructed index in {}'.format(time.time()-start))
-    # pp = pprint.PrettyPrinter(indent=4)
-    # pp.pprint(indexer.inverted_index_list)
     with open('inverted_index.json', 'w') as fp:
         json.dump(indexer.inverted_index_list, fp)
 
